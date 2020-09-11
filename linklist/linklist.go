65378b0c0c028de6d008187f150fb5a6f6da3111
chonlatee <jumratsee@gmail.com>
2020-09-11 18:17:45 +0700

    finish link single link list

--- linklist/linklist.go
+++ linklist/linklist.go
@@ -1,6 +1,9 @@
 package linklist
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 type elm struct {
 	val  interface{}
@@ -10,49 +13,184 @@ type elm struct {
 type SingleLinklist struct {
 	len  int
 	head *elm
+	tail *elm
 }
 
 func (s *SingleLinklist) InsertFront(val interface{}) {
-
+	l := &elm{
+		val: val,
+	}
+	if s.IsEmpty() {
+		s.head = l
+		s.tail = l
+	} else {
+		l.next = s.head
+		s.head = l
+	}
+	s.len++
 }
 
 func (s *SingleLinklist) InsertBack(val interface{}) {
+	l := &elm{
+		val: val,
+	}
 
+	if s.IsEmpty() {
+		s.head = l
+		s.tail = l
+	} else {
+		s.tail.next = l
+		s.tail = l
+	}
+
+	s.len++
 }
 
+// InsertBefore insert before found value InsertBefore(value, before interface{})
 func (s *SingleLinklist) InsertBefore(val, before interface{}) {
 
+	l := &elm{
+		val: val,
+	}
+	prev := s.head
+	current := s.head
+
+	for current != nil && current.val != before {
+
+		prev = current
+		current = current.next
+	}
+
+	if current == nil {
+		log.Fatalln("Item not found")
+	}
+
+	prev.next = l
+	l.next = current
+
+	s.len++
 }
 
+// InsertAfter insert after found value InsertAfter(value, after interface{})
 func (s *SingleLinklist) InsertAfter(val, after interface{}) {
 
+	l := &elm{
+		val: val,
+	}
+
+	current := s.head
+	prev := s.head
+
+	for current != nil && current.val != after {
+
+		prev = current
+		current = current.next
+	}
+
+	if current == nil {
+		log.Fatalln("Item not found")
+	}
+
+	if current.val == s.tail.val {
+		s.tail.next = l
+		s.tail = l
+	} else {
+		l.next = current.next
+		prev.next = l
+
+	}
+
+	s.len++
 }
 
 func (s *SingleLinklist) RemoveFront() {
-
+	if s.IsEmpty() {
+		log.Fatalln("Link list is empty.")
+	}
+	s.head = s.head.next
+	s.len--
 }
 
 func (s *SingleLinklist) RemoveBack() {
-
+	prev := s.head
+	current := s.head
+	for current.next != nil {
+		prev = current
+		current = current.next
+	}
+	s.tail = prev
+	s.tail.next = nil
+	s.len--
 }
 
-func (s *SingleLinklist) RemoveBefore(val, after interface{}) {
+// RemoveBefore use for remove node before given value RemoveBefore(before interface{})
+func (s *SingleLinklist) RemoveBefore(before interface{}) {
 
+	current := s.head
+	target := s.head
+	prevTarget := s.head
+
+	for current != nil && target.next.val != before && current.next != nil {
+		prevTarget = target
+		target = current
+		current = current.next
+	}
+
+	if current == nil {
+		log.Fatalln("Item not found")
+	}
+
+	prevTarget.next = target.next
+
+	s.len--
 }
 
-func (s *SingleLinklist) RemoveAfter(val, after interface{}) {
+func (s *SingleLinklist) RemoveAfter(after interface{}) {
 
+	current := s.head
+	prev := s.head
+
+	for current != nil && prev.val != after {
+		prev = current
+		current = current.next
+	}
+
+	if current == nil {
+		log.Fatalln("Item not found")
+	}
+
+	prev.next = current.next
+
+	if prev.next == nil {
+		s.tail = prev
+	}
+
+	s.len--
 }
 
 func (s *SingleLinklist) Traverse() {
 	current := s.head
 	for current != nil {
-		fmt.Println(current.val)
+		fmt.Printf("%d ", current.val)
+		current = current.next
 	}
+	fmt.Println()
 }
 
 func (s *SingleLinklist) Size() int {
 	return s.len
+}
+
+func (s *SingleLinklist) IsEmpty() bool {
+	return s.head == nil && s.tail == nil && s.len == 0
+}
+
+func (s *SingleLinklist) ShowHeadValue() {
+	fmt.Println(s.head.val)
+}
+
+func (s *SingleLinklist) ShowTailvalue() {
+	fmt.Println(s.tail.val)
 }
 
 func NewSingleLinklist() *SingleLinklist {
--- main.go
+++ main.go
@@ -3,28 +3,65 @@ package main
 import (
 	"fmt"
 
+	"github.com/chonlatee/simpleds/linklist"
+
 	"github.com/chonlatee/simpleds/queue"
 	"github.com/chonlatee/simpleds/stack"
 )
 
 func main() {
 	mystack := stack.NewStack(10)
-	fmt.Println(mystack.Size())
+	// fmt.Println(mystack.Size())
 	mystack.Push(10)
-	fmt.Println(mystack.Size())
-	fmt.Println(mystack.Peek())
-	fmt.Println(mystack.Pop())
-	fmt.Println(mystack.Size())
-	fmt.Println(mystack.Pop())
+	// fmt.Println(mystack.Size())
+	// fmt.Println(mystack.Peek())
+	// fmt.Println(mystack.Pop())
+	// fmt.Println(mystack.Size())
 
 	myqueue := queue.NewQueue(10)
 	myqueue.EnQueue(4)
-	myqueue.EnQueue(2)
-	myqueue.EnQueue(7)
+	// myqueue.EnQueue(2)
+	// myqueue.EnQueue(7)
 
-	fmt.Println(myqueue.DeQueue())
-	fmt.Println(myqueue.DeQueue())
-	fmt.Println(myqueue.Peek())
-	fmt.Println(myqueue.IsEmpty())
-	fmt.Println(myqueue.IsFull())
+	// fmt.Println(myqueue.DeQueue())
+	// fmt.Println(myqueue.DeQueue())
+	// fmt.Println(myqueue.Peek())
+	// fmt.Println(myqueue.IsEmpty())
+	// fmt.Println(myqueue.IsFull())
+
+	fmt.Println("Linklist")
+
+	myLinklist := linklist.NewSingleLinklist()
+
+	// myLinklist.InsertFront(10)
+	// myLinklist.InsertFront(9)
+	// myLinklist.InsertFront(8)
+	// myLinklist.InsertFront(7)
+
+	// myLinklist.InsertBack(10)
+	// myLinklist.InsertBack(9)
+	// myLinklist.InsertBack(7)
+	// myLinklist.Traverse()
+	// myLinklist.InsertBefore(8, 7)
+	// myLinklist.Traverse()
+	// myLinklist.InsertAfter(100, 10)
+	// myLinklist.Traverse()
+	// myLinklist.InsertAfter(6, 7)
+	// myLinklist.Traverse()
+	// myLinklist.RemoveBack()
+	// myLinklist.Traverse()
+	// fmt.Println()
+	// myLinklist.RemoveBefore(7)
+	// fmt.Println()
+	// myLinklist.Traverse()
+	// myLinklist.RemoveAfter(9)
+	// myLinklist.Traverse()
+
+	// myLinklist.ShowHeadValue()
+	// myLinklist.ShowTailvalue()
+	// myLinklist.InsertAfter(8, 9)
+	// myLinklist.Traverse()
+	// myLinklist.ShowHeadValue()
+	// myLinklist.ShowTailvalue()
+
 }
